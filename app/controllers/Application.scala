78322cb39595f160b121d88ce6eb1bf05c14a02d
Misha wagner <mishajw@gmail.com>
2016-02-22 11:20:17 +0000

    Started simple JSON request template

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,13 @@
 package controllers
 
-import play.api._
+import gamblinganalysis.odds.OddsCollection
+import org.json4s._
+import org.json4s.jackson.JsonMethods
+
+import gamblinganalysis.analysis.OddsOptimiser
+import gamblinganalysis.retriever.GameRetriever
+import gamblinganalysis.retriever.odds.OddsCheckerRetriever
+import gamblinganalysis.util.exceptions.ParseException
 import play.api.mvc._
 
 object Application extends Controller {
@@ -9,4 +16,36 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def bestOdds = Action {
+    val bestOdds = GameRetriever.retrieve.flatMap(g => {
+      try {
+        val odds = OddsCheckerRetriever.getOdds(g)
+        Some(OddsOptimiser.optimise(odds))
+      } catch {
+        case e: Exception => None
+        case e: ParseException => None
+      }
+    })
+
+    val json = JObject(List(
+      "oddsCollection" ->
+        JArray(bestOdds.toList.map(parseOddsCollection))
+    ))
+
+    Ok(JsonMethods.pretty(JsonMethods.render(json)))
+  }
+
+  private def parseOddsCollection(oc: OddsCollection) = {
+    JObject(List(
+      "odds" -> JArray(oc.odds.toList.map(o => {
+        JObject(List(
+          "numerator" -> JInt(o.gains),
+          "denominator" -> JInt(o.base),
+          "bookie" -> JString(o.bookie.name),
+          "outcome" -> JString(o.outcome)
+        ))
+      })),
+      "roi" -> JDecimal(oc.getInvestmentReturn)
+    ))
+  }
 }
