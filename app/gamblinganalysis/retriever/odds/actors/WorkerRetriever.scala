66ea8530dbcd266b89f2614e79064aece3f7bab2
Misha wagner <mishajw@gmail.com>
2016-02-24 16:51:22 +0000

    Adds to database on retreive

--- app/gamblinganalysis/retriever/odds/actors/WorkerRetriever.scala
+++ app/gamblinganalysis/retriever/odds/actors/WorkerRetriever.scala
@@ -1,6 +1,7 @@
 package gamblinganalysis.retriever.odds.actors
 
 import akka.actor.Actor
+import gamblinganalysis.util.db.GameDetailsDBHandler
 import play.api.Logger
 
 /**
@@ -13,7 +14,11 @@ class WorkerRetriever extends Actor {
   def receive = {
     case ScrapingWork(work) =>
       log.debug(s"Got work: $work")
-      work.retrieve()
+      val oddsCollection = work.retrieve()
+
+      log.debug(s"Inserting ${oddsCollection.odds.size} into database")
+      oddsCollection.odds.foreach(GameDetailsDBHandler.insertOdd)
+
       log.debug(s"Done with work: $work")
       sender() ! DoneScraping(work)
     case NoMoreWork() =>
