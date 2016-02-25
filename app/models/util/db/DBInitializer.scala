48cb082bd20c4cf2265a1070a3a3591febd7adc6
Misha wagner <mishajw@gmail.com>
2016-02-25 16:31:17 +0000

    Fixed error in detecting if account has already been added

--- app/models/util/db/DBInitializer.scala
+++ app/models/util/db/DBInitializer.scala
@@ -1,8 +1,27 @@
 package models.util.db
 
+import models.{Account, Bookie, User}
+import org.json4s.JsonAST._
+import org.json4s.native.JsonParser
+
+import scala.io.Source
+
 /**
   * Created by misha on 25/02/16.
   */
 object DBInitializer {
+  def fillWithJson() = {
+    val json = JsonParser.parse(Source.fromFile("res/json/start-up.json").mkString)
 
+    val accounts: Seq[Account] = for (
+      JObject(obj) <- json;
+      JField("accounts", JArray(accounts)) <- obj;
+      JObject(fields) <- accounts;
+      JField("user", JString(user)) <- fields;
+      JField("bookie", JString(bookie)) <- fields;
+      JField("amount", JDouble(amount)) <- fields
+    ) yield Account(User(user), amount, Bookie(bookie))
+
+    accounts foreach UserDBHandler.insertAccount
+  }
 }
