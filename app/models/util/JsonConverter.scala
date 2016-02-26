3086df8d82c4227fb9b5b292b242053392fde5a6
Misha wagner <mishajw@gmail.com>
2016-02-26 16:14:20 +0000

    JsonConverter can now convert both ways

--- app/models/util/JsonConverter.scala
+++ app/models/util/JsonConverter.scala
@@ -1,6 +1,6 @@
 package models.util
 
-import models.Account
+import models._
 import models.analysis.BuyingPlan
 import models.odds.Odd
 import org.json4s.JsonAST.{JDecimal, JString, JObject}
@@ -12,8 +12,39 @@ object JsonConverter {
       "numerator" -> JInt(odd.gains),
       "denominator" -> JInt(odd.base),
       "bookie" -> JString(odd.bookie.name),
-      "outcome" -> JString(odd.outcome)
+      "outcome" -> JString(odd.outcome),
+      "game" -> fromGame(odd.game)
     ))
+  }
+
+  def toOdd(json: JObject): Odd = {
+    {for {
+      JObject(obj) <- json
+      JField("numerator", JInt(numerator)) <- obj
+      JField("denominator", JInt(denominator)) <- obj
+      JField("bookie", JString(bookie)) <- obj
+      JField("outcome", JString(outcome)) <- obj
+      JField("game", game: JObject) <- obj
+    } yield new Odd(numerator.toInt, denominator.toInt, outcome, toGame(game), Bookie(bookie))}
+        .asInstanceOf[List[Odd]]
+        .head
+  }
+
+  def fromGame(game: Game): JObject = {
+    JObject(List(
+      "outcomes" -> JArray(game.outcomes.map(JString).toList),
+      "sport" -> JString(game.sport.title)
+    ))
+  }
+
+  def toGame(json: JObject): Game = {
+    {for {
+      JObject(obj) <- json
+      JField("outcomes", JArray(outcomes: List[JString])) <- obj
+      JField("sport", JString(sport)) <- obj
+    } yield Game(outcomes.map({case JString(s) => s}).toSet, Sport(sport))}
+        .asInstanceOf[List[Game]]
+        .head
   }
 
   def fromAccount(account: Account) = {
@@ -22,6 +53,17 @@ object JsonConverter {
       "amount" -> JDecimal(account.amount),
       "bookie" -> JString(account.bookie.name)
     ))
+  }
+
+  def toAccount(json: JObject): Account = {
+    {for {
+      JObject(obj) <- json
+      JField("user", JString(user)) <- obj
+      JField("amount", JDecimal(amount)) <- obj
+      JField("bookie", JString(bookie)) <- obj
+    } yield Account(User(user), amount, Bookie(bookie))}
+        .asInstanceOf[List[Account]]
+        .head
   }
 
   def fromBuyingPlan(plan: BuyingPlan) = {
@@ -35,5 +77,21 @@ object JsonConverter {
       })),
       "roi" -> JDecimal(plan.roi)
     ))
+  }
+
+  def toBuyingPlan(json: JObject) = {
+    {for {
+      JObject(obj) <- json
+      JField("pairs", JArray(pairs)) <- obj
+    } yield {
+      val odds = for {
+        JObject(pair) <- pairs
+        JField("odd", odd: JObject) <- pair
+        JField("money", JDecimal(money)) <- pair
+        JField("account", account: JObject) <- pair
+      } yield (toOdd(odd), Some(money), Some(toAccount(account)))
+      new BuyingPlan(odds)
+    }}.asInstanceOf[List[BuyingPlan]]
+      .head
   }
 }
